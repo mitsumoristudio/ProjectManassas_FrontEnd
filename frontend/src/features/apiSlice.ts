da22b8f5397a56d9a8f73c4f08953c1f1a58bdcf
satoshimitsumori <smitsumori@hotmail.com>
2025-08-31 13:32:33 -0500

    added changes to package.json proxy

--- frontend/src/features/apiSlice.ts
+++ frontend/src/features/apiSlice.ts
@@ -3,10 +3,12 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {PUB_URL} from "../util/urlconstants";
 
+
 // Development && Published URL
 const baseQuery = fetchBaseQuery({
  //   baseUrl: BASE_URL
     baseUrl: PUB_URL,
+
 });
 
 export const apiSlice = createApi({
